bddeb25951e296d4f4245d0bec614f8ecb0a8365
TcdGG <78429664+TcdGG@users.noreply.github.com>
2021-06-15 12:09:49 +0200

    Translation
    Translated from German to english

---
+++ Comments.java
@@ -0,0 +1,9 @@
+public class Comments{
+    public static void main (Stringargs[]){
+        //I am a comment
+        System.out.println("Hello, I am able to see me");
+        //System.out.println("Hello, I am able to see me");
+        /* I am a comment
+       over more than one code line */
+    }
+}
--- public class Kommentare.java
+++
@@ -1,9 +0,0 @@
-public class Kommentare{
-    public static void main (Stringargs[]){
-        //Ich bin ein Kommentare
-        System.out.println("Hallo, mich kann man sehen!");
-        //System.out.println("Hallo, mich kann man nicht sehen");
-        /* Ich bin ein Kommentar
-        über mehrere Codezeilen hinweg */
-    }
-}
